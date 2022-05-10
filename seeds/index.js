2930e6826ee7e9d70edf8240c7dd94ac5db83133
Joseph Murray <josephl.murrayjr@gmail.com>
2022-05-10 10:19:28 -0400

    seed push

--- seeds/index.js
+++ seeds/index.js
@@ -3,9 +3,12 @@ const sequelize = require('../config/connection');
 // const teamData = require('./teamData.json');
 const seedPlayers = require('./newPlayers');
 const seedTeams = require('./teamData');
+const seedUsers = require('./userData')
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+
+  await seedUsers();
 
   await seedPlayers();
 
