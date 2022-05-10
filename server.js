cb1a4d005207fd5b418d4abd6be30ac5cbe6a1f3
Rene Ortega <reneortega2468@gmail.com>
2022-05-10 13:10:18 -0400

    working

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const routes = require('./controllers');
+
 const helpers = require('./utils/helpers');
 
-var mysql = require('mysql2');
 
-const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,21 +27,27 @@ const sess = {
 
 app.use(session(sess));
 
+
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
-    console.log(
-      '`\nServer running on port ${PORT}. Visit http://localhost:3001 and create an account!`'
-    )
-  );
+// esquelize.sync({ force: true }).then(() => {
+//   app.listen(PORT, () =>
+//     console.log(
+//       '`\nServer running on port ${PORT}. Visit http://localhost:3001 and create an account!`'
+//     )
+//   );
+// });
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
