14f558f02d47a7d0e5ba6396c613a09d1d5d98ba
Rene Ortega <reneortega2468@gmail.com>
2022-05-11 11:05:35 -0400

    log in and landing page done.

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 
+// app.use("/images", express.static(path.join(__dirname, "/public/images")));
+
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
