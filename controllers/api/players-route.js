638226406be7ecea7f2b25640ae80b6e2e038870
Rene Ortega <reneortega2468@gmail.com>
2022-05-11 20:46:06 -0400

    idk

--- controllers/api/players-route.js
+++ controllers/api/players-route.js
@@ -7,48 +7,4 @@ router.get('/', (req, res) => {
 })
 
 
-
-
-
-router.get('/users:PlayerID', async (req, res) => {
-  try {
-    const playerData = await Player.findAll({ where: { userId: req.params.PlayerID}});
-  const players = playerData.map(play => play.get({
-    plain: true
-  }));
-    res.render('dashboard', {players});
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
-
-router.get('/', async (req, res) => {
-  try { 
-    const playerData = await Player.findAll({});
-      const players = playerData.map(play => play.get({plain: true}));
-      res.render('dashboard', {players});
-  } catch (err) {
-    res.status(400).json(err);
-  }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = router;
